6890648d637aeb28a4c140f9ba5a44619c9ec7f5
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-03 14:19:41 -0600

    Creating AddFriendButton component for Dashboard

---
+++ blisscord/src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <main>
+      <div>dashboard page</div>
+    </main>
+  )
+}
+
+export default page
