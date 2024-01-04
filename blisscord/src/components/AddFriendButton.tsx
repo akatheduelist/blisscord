3d6a54b2803e2e5794803af5fb07816b06164643
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-03 20:19:02 -0600

    Fixing issue with email input register

--- blisscord/src/components/AddFriendButton.tsx
+++ blisscord/src/components/AddFriendButton.tsx
@@ -10,10 +10,10 @@ import { useForm } from 'react-hook-form'
 
 interface AddFriendButtonProps {}
 
+type FormData = z.infer<typeof addFriendValidator>
+
 const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
   const [showSuccessState, setShowSuccessState] = useState<boolean>(false)
-
-  type FormData = z.infer<typeof addFriendValidator>
 
   const { register, handleSubmit, setError, formState: {errors} } = useForm<FormData>({
     resolver: zodResolver(addFriendValidator)
@@ -22,10 +22,12 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
   const addFriend = async (email: string) => {
     try {
       // Is the email valid? If so, post to the database.
-      const validatedEmail = addFriendValidator.parse({email}) 
+      const validatedEmail = addFriendValidator.parse({email})
+
       await axios.post('/api/friends/add',  {
         email: validatedEmail,
         })
+
       setShowSuccessState(true)
       } catch (error) {
       // ERROR => Handle errors with validations 
@@ -48,7 +50,7 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
     addFriend(data.email)
   }
 
-  return <>
+  return ( 
     <form onSubmit={handleSubmit(onSubmit)} className='max-w-sm'>
       <label htmlFor='email'
       className='block text-sm font-medium leading-6 text-gray-900'>
@@ -56,7 +58,10 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
       </label>
       <div className='mt-2 flex gap-4'>
         <input
-          type='text' className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus: ring-indigo-600 sm:text-sm sm: leading-6' placeholder='you@example.com' />
+          {...register('email')}
+          type='text' 
+          className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus: ring-indigo-600 sm:text-sm sm: leading-6' 
+          placeholder='you@example.com' />
         <Button>Add</Button>
       </div>
       <p className='mt-1 text-sm text-red-600'>{errors.email?.message}</p>
@@ -64,7 +69,7 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
       <p className='mt-1 text-sm text-green-600'>Friend request sent</p>
       ) : null}
     </form>
-  </>
+  )
 }
 
 export default AddFriendButton
