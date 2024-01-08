a2ae190fb680fcf7702f86efc56b9a60dfc3e690
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-08 13:45:18 -0600

    Reapplying page elements after fixing nesting issue

--- blisscord/src/app/(dashboard)/dashboard/layout.tsx
+++ blisscord/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { Icons } from "@/components/Icons";
+import { Icon, Icons } from "@/components/Icons";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
@@ -8,6 +8,22 @@ import { ReactNode } from "react";
 interface layoutProps {
   children: ReactNode;
 }
+
+interface SidebarOption {
+  id: number;
+  name: string;
+  href: string;
+  Icon: Icon;
+}
+
+const sidebarOptions: SidebarOption[] = [
+  {
+    id: 1,
+    name: "Add friend",
+    href: "/dashboard/add",
+    Icon: "UserPlus",
+  },
+];
 
 const layout = async ({ children }: layoutProps) => {
   const session = await getServerSession(authOptions);
@@ -23,6 +39,38 @@ const layout = async ({ children }: layoutProps) => {
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
           <Icons.Logo className="h-8 w-auto text-indigo-600" />
         </Link>
+
+        <div className="text-xs fond-semibold leading-6 text-gray-400">
+          Your chats
+        </div>
+        <nav className="flex flex-1 flex-col">
+          <ul role="list" className="flex flex-1 flex-col gap-y-7">
+            <li>YOUR CHATS HERE</li>
+            <li>
+              <div className="text-xs font-semibold leading-6 text-gray-400">
+                Overview
+              </div>
+              <ul role="list" className="-mx-2 mt-2 space-y-1">
+                {sidebarOptions.map((option) => {
+                  const Icon = Icons[option.Icon];
+                  return (
+                    <li key={option.id}>
+                      <Link
+                        href={option.href}
+                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md padding-2 text-sm leading-6 font-semibold"
+                      >
+                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border-text-[0.625rem] font-md bg-white">
+                          <Icon className="h-4 w-4" />
+                        </span>
+                        <span className="truncate">{option.name}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          </ul>
+        </nav>
       </div>
       {children}
     </div>
