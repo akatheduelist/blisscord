966ea5f33bf471836fd62b57d26fb21e4d6872a8
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-08 14:18:49 -0600

    Creating signout button component with error handling

--- blisscord/src/components/SignOutButton.tsx
+++ blisscord/src/components/SignOutButton.tsx
@@ -1,11 +1,37 @@
-"use client"
+"use client";
 
-import { FC } from 'react'
+import { ButtonHTMLAttributes, FC, useState } from "react";
+import Button from "./ui/Button";
+import toast from "react-hot-toast";
+import { signOut } from "next-auth/react";
+import { Loader2, LogOut } from "lucide-react";
 
-interface SignOutButtonProps {}
+interface SignOutButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const SignOutButton: FC<SignOutButtonProps> = ({}) => {
-  return <div>SignOutButton</div>
-}
+const SignOutButton: FC<SignOutButtonProps> = ({ ...props }) => {
+  const [isSigningOut, setIsSigningOut] = useState<boolean>(false);
+  return (
+    <Button
+      {...props}
+      variant="ghost"
+      onClick={async () => {
+        setIsSigningOut(true);
+        try {
+          await signOut();
+        } catch (error) {
+          toast.error("There was a problem signing out");
+        } finally {
+          setIsSigningOut(false);
+        }
+      }}
+    >
+      {isSigningOut ? (
+        <Loader2 className="animate-spin h4 w-4" />
+      ) : (
+        <LogOut className="w-4 h-4" />
+      )}
+    </Button>
+  );
+};
 
-export default SignOutButton
+export default SignOutButton;
