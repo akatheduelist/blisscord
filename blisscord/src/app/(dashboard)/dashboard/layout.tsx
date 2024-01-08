85aa2bd03fe44c5d1e0ce36486cd95c40ae38d34
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-08 16:11:19 -0600

    Adding logic for pending friend requests to display in sidebar

--- blisscord/src/app/(dashboard)/dashboard/layout.tsx
+++ blisscord/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,11 +1,12 @@
 import { Icon, Icons } from "@/components/Icons";
-import Image from 'next/image'
+import Image from "next/image";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { ReactNode } from "react";
 import SignOutButton from "@/components/SignOutButton";
+import FriendRequestSidebarOptions from "@/components/FriendRequestSidebarOptions";
 
 interface layoutProps {
   children: ReactNode;
@@ -34,6 +35,7 @@ const layout = async ({ children }: layoutProps) => {
 
   return (
     <div className="w-full flex h-screen">
+      <div></div>
       <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
           <Icons.Logo className="h-8 w-auto text-indigo-600" />
@@ -68,24 +70,27 @@ const layout = async ({ children }: layoutProps) => {
                 })}
               </ul>
             </li>
-              
-
-
+            <li><FriendRequestSidebarOptions /></li>
             <li className="-mx-6 mt-auto flex items-center">
-              <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
+              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
                 <div className="relative h-8 w-8 bg-gray-50">
-                  <Image 
+                  <Image
                     fill
-                    referrerPolicy='no-referrer' 
-                    className='rounded-full' src={session.user.image || ''} 
-                    alt="Profile picture" />
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
+                    src={session.user.image || ""}
+                    alt="Profile picture"
+                  />
                 </div>
-                <span className='sr-only'>Your Profile</span>
-                <div className='flex flex-col'>
-                <span aria-hidden='true'>{session.user.name}</span></div>
-                <span className='text-xs text-zinc-400' aria-hidden="true">{session.user.email}</span>
+                <span className="sr-only">Your Profile</span>
+                <div className="flex flex-col">
+                  <span aria-hidden="true">{session.user.name}</span>
+                </div>
+                <span className="text-xs text-zinc-400" aria-hidden="true">
+                  {session.user.email}
+                </span>
               </div>
-              <SignOutButton className='h-f aspect-square' />
+              <SignOutButton className="h-f aspect-square" />
             </li>
           </ul>
         </nav>
