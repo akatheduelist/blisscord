bb558f9024a65b8f4967fed37b1e9a677736acd4
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-08 14:04:57 -0600

    Adding google user image domain to NextJS config

--- blisscord/src/app/(dashboard)/dashboard/layout.tsx
+++ blisscord/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { Icon, Icons } from "@/components/Icons";
+import Image from 'next/image'
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
@@ -32,10 +33,7 @@ const layout = async ({ children }: layoutProps) => {
 
   return (
     <div className="w-full flex h-screen">
-      <div
-        className="flex h-full w-full max-w-xs grow flex-col gap-y-5 
-        overflow-y-auto border-r border-gray-200 bg-white px-6"
-      >
+      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
           <Icons.Logo className="h-8 w-auto text-indigo-600" />
         </Link>
@@ -68,6 +66,21 @@ const layout = async ({ children }: layoutProps) => {
                   );
                 })}
               </ul>
+            </li>
+            <li className="-mx-6 mt-auto flex items-center">
+              <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
+                <div className="relative h-8 w-8 bg-gray-50">
+                  <Image 
+                    fill
+                    referrerPolicy='no-referrer' 
+                    className='rounded-full' src={session.user.image || ''} 
+                    alt="Profile picture" />
+                </div>
+                <span className='sr-only'>Your Profile</span>
+                <div className='flex flex-col'>
+                <span aria-hidden='true'>{session.user.name}</span></div>
+                <span className='text-xs text-zinc-400' aria-hidden="true">{session.user.email}</span>
+              </div>
             </li>
           </ul>
         </nav>
