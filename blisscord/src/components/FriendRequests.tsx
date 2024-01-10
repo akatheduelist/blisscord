e56b789b9b4261cfba070bafd6f72f12b06b40dc
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-10 14:21:07 -0600

    Adding api request for friend request/deny functions

--- blisscord/src/components/FriendRequests.tsx
+++ blisscord/src/components/FriendRequests.tsx
@@ -2,17 +2,35 @@
 
 import { Check, UserPlus, X } from "lucide-react";
 import { FC, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 interface FriendRequestsProps {
   incomingFriendRequests: IncomingFriendRequest[];
   sessionId: string;
 }
 
-const FriendRequests: FC<FriendRequestsProps> = ({ incomingFriendRequests, sessionId }) => {
+const FriendRequests: FC<FriendRequestsProps> = ({
+  incomingFriendRequests,
+  sessionId,
+}) => {
+  const router = useRouter();
 
   const [friendRequests, setFriendRequest] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests,
   );
+
+  const acceptFriend = async (senderId: string) => {
+    await axios.post("/api/requests/accept", {
+      id: senderId,
+    });
+
+    setFriendRequest((prev) =>
+      prev.filter((request) => request.senderId !== senderId),
+    );
+
+    router.refresh();
+  };
 
   return (
     <>
@@ -25,11 +43,17 @@ const FriendRequests: FC<FriendRequestsProps> = ({ incomingFriendRequests, sessi
 
             <p className="font-medium text-lg">{request.senderEmail}</p>
 
-            <button aria-label="accept friend" className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-item-center rounded-full transition hover:shadow-md">
+            <button
+              aria-label="accept friend"
+              className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-item-center rounded-full transition hover:shadow-md"
+            >
               <Check className="font-semibold text-white w-3/4 h-3/4" />
             </button>
 
-            <button aria-label="deny friend" className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-item-center rounded-full transition hover:shadow-md">
+            <button
+              aria-label="deny friend"
+              className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-item-center rounded-full transition hover:shadow-md"
+            >
               <X className="font-semibold text-white w-3/4 h-3/4" />
             </button>
           </div>
