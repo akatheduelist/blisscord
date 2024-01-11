71993c9771b166aef98681d13d3dcd0d76cc872d
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-11 13:59:28 -0600

    Adding chat component with chatID params

---
+++ blisscord/src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -0,0 +1,32 @@
+import { fetchRedis } from "@/app/helpers/redis";
+import { FC } from "react";
+
+interface PageProps {
+  params: {
+    chatId: string;
+  };
+}
+
+async function getChatMessages(chatId: string) {
+  try {
+    const results: string[] = await fetchRedis(
+      "zrange",
+      `chat:${chatId}:messages`,
+      0,
+      -1,
+    );
+  } catch (error) {
+    notFound();
+  }
+}
+
+const dbMessages = results.map((message) => JSON.parse(message) as Message);
+
+// Gets the 'chatId' params from the [chatId] in the routing structure
+const page = async ({ params }: PageProps) => {
+  const { chatId } = params;
+
+  return <div>{params.chatId}</div>;
+};
+
+export default page;
--- blisscord/src/types/db.d.ts
+++ blisscord/src/types/db.d.ts
@@ -1,7 +1,26 @@
-// Define DB types so they don't need to be imported around the app
+// Define DB types so they don't have to be defined around the app
 interface User {
-  name: string
-  email: string
-  id: string
-  image: string
+  name: string;
+  email: string;
+  id: string;
+  image: string;
+}
+
+interface Chat {
+  id: string;
+  messages: Message[];
+}
+
+interface Message {
+  id: string;
+  senderId: string;
+  receiverId: string;
+  text: string;
+  timestamp: number;
+}
+
+interface FriendRequest {
+  id: string;
+  senderID: string;
+  receiverId: string;
 }
