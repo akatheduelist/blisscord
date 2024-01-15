d7fa9180d7bdea357ccc6b2f4d2a76e53af986df
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-15 16:56:24 -0600

    Adding basic styling to chat window area of chat component, including image of chat partner

--- blisscord/src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ blisscord/src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,5 +1,10 @@
 import { fetchRedis } from "@/app/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { messageArrayValidator } from "@/lib/validations/message";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import Image from "next/image";
 import { FC } from "react";
 
 interface PageProps {
@@ -28,8 +33,41 @@ async function getChatMessages(chatId: string) {
 // Gets the 'chatId' params from the [chatId] in the routing structure
 const page = async ({ params }: PageProps) => {
   const { chatId } = params;
+  const session = await getServerSession(authOptions);
 
-  return <div>{params.chatId}</div>;
+  if (!session) notFound();
+
+  const { user } = session;
+
+  const [userId1, userId2] = chatId.split("--");
+
+  if (user.id !== userId1 && user.id !== userId2) {
+    notFound();
+  }
+
+  const chatPartnerId = user.id === userId1 ? userId2 : userId1;
+  const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
+  const initialMessages = await getChatMessages(chatId);
+
+  return (
+    <div className="flex-1 justify-between flex felx-col h-full max-h-[calc(100vh-6rem)]">
+      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative flex items-center space-x-4">
+          <div className="relative">
+            <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+              <Image
+                fill
+                referrerPolicy="no-referrer"
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className="rounded-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
