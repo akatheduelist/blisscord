5dbb3ebbaf9a4baf27b6d0078950820dcfce6179
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-15 16:35:34 -0600

    Fixing number of missed messages to be displayed in side bar. Sidebar displays user name of friends currently chatting with. Updated styling for name display in sidebar

--- blisscord/src/components/SidebarChatList.tsx
+++ blisscord/src/components/SidebarChatList.tsx
@@ -1,11 +1,15 @@
+"use client";
+
+import { chatHrefConstructor } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
-import { FC, useEffect, useState } from "react";
+import { FC, Key, useEffect, useState } from "react";
 
 interface sidebarChatListProps {
   friends: User[];
+  sessionId: string;
 }
 
-const SidebarChatList: FC<sidebarChatListProps> = ({ }) => {
+const SidebarChatList: FC<sidebarChatListProps> = ({ friends, sessionId }) => {
   const router = useRouter();
   const pathname = usePathname();
   const [unseenMessages, setUnseenMessages] = useState<Message[]>([]);
@@ -19,9 +23,31 @@ const SidebarChatList: FC<sidebarChatListProps> = ({ }) => {
   }, [pathname]);
 
   return (
-    <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
-      {/* {friends.sort().map} */}
-    </ul>
+    <div>
+      <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
+        {friends.sort().map((friend) => {
+          const unseenMessagesCount = unseenMessages.filter((unseenMsg) => {
+            return unseenMsg.senderId === friend.id;
+          }).length;
+
+          return (
+            <li key={friend.id}>
+              <a
+                className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                href={`/dashboard/chat/${chatHrefConstructor(sessionId, friend.id)}`}
+              >
+                {friend.name}
+                {unseenMessagesCount > 0 ? (
+                  <div className="bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center">
+                    {unseenMessagesCount}
+                  </div>
+                ) : null}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
